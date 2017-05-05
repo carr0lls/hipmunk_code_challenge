20e501496bee3eb422fc1c2d7bf3744f4fca75ee
Carroll Yu <carrollcyu@gmail.com>
2017-05-04 17:16:03 -0700

    refactor promises

--- server.js
+++ server.js
@@ -11,78 +11,87 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// API routes
 app.get('/api', function(req, res) {
 	res.end('<div>Hipmunk Search API coding challenge</div>');
 });
-// API routes
 app.get('/hotels/search', function(req, res) {
-	getHotels(function(err, data) {
-		if (err)
-			res.status(500).json({ error: err.reason });
-		else
+	getHotels(PROVIDERS)
+		.then(mergeData)
+		.then(function(data) {
 			res.json(data);
-	});
+		})
+    	.catch(function(err) {
+    		res.status(500).json({ error: err.reason });
+    	});
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+  console.log('Hipmunk - Hotel Search API: http://localhost:' + app.get('port') + '/');
 });
 
+function scrapeData(provider) {
+	return function(resolve, reject) {
+		request.get('http://localhost:9000/scrapers/'+provider).end(function(err, result) {
+    		if (err) {
+				// console.log('failed to retrieve '+provider+' hotel list', err);
+				let errorObject = { 
+					    reason: 'failed to retrieve '+provider+' hotel list',
+					error: err
+				}
+				return reject(errorObject);
+        	}
+        	else {
+        		resolve({ data: result.body.results, index: 0 });
+        	}
+    	});
+	}		
+}
 
-function getHotels(cb) {
+function buildHotelPromises(providers) {
 	let promises = [];
 
-	for (let provider of PROVIDERS) {
+	for (let provider of providers) {
 		promises.push(new Promise(scrapeData(provider)));
 	}
 
-	function scrapeData(provider) {
-		return function(resolve, reject) {
-			request.get('http://localhost:9000/scrapers/'+provider).end(function(err, result) {
-	    		if (err) {
-					// console.log('failed to retrieve '+provider+' hotel list', err);
-					cb({ reason: 'failed to retrieve '+provider+' hotel list', error: err }, null);
-	        	}
-	        	else {
-	        		resolve({ data: result.body.results, index: 0 });
-	        	}
-	    	});
-		}		
-	}
+	return promises;
+}
 
-	function mergeData(providerData) {
-		let hotelData = { results: [] }, length = 0;
-		let pdata, highestEcstasyData, highestEcstasyProvider;
+function getHotels(providers) {
+	return Promise.all(buildHotelPromises(providers));
+}
 
-		for (let provider in providerData) 
-			length += providerData[provider]['data'].length;
+function mergeData(providerData) {
+	let hotelData = { results: [] }, mergedLength = 0;
+	let pdata, highestEcstasyData, highestEcstasyProvider;
 
-		for (let i=0; i < length; i++) {			
-			highestEcstasyData = null;
-			highestEcstasyProvider = null;
+	for (let provider of providerData) 
+		mergedLength += provider['data'].length;
+
+	for (let i=0; i < mergedLength; i++) {			
+		highestEcstasyData = null;
+		highestEcstasyProvider = null;
+		
+		for (let provider in providerData) {
+			pdata = providerData[provider]['data'][providerData[provider]['index']];
+
+			if (providerData[provider]['index'] === providerData[provider]['data'].length)
+				continue;
 			
-			for (let provider in providerData) {
-				pdata = providerData[provider]['data'][providerData[provider]['index']];
-
-				if (providerData[provider]['index'] === providerData[provider]['data'].length)
-					continue;
-				
-				if (highestEcstasyData === null) {
-					highestEcstasyData = pdata;
-					highestEcstasyProvider = provider;
-				}
-				else if (pdata.ecstasy > highestEcstasyData.ecstasy) {
-					highestEcstasyData = pdata;
-					highestEcstasyProvider = provider;
-				}
+			if (highestEcstasyData === null) {
+				highestEcstasyData = pdata;
+				highestEcstasyProvider = provider;
 			}
-
-			hotelData.results.push(highestEcstasyData);
-			providerData[highestEcstasyProvider]['index']++;
+			else if (pdata.ecstasy > highestEcstasyData.ecstasy) {
+				highestEcstasyData = pdata;
+				highestEcstasyProvider = provider;
+			}
 		}
 
-		cb(null, hotelData);
+		hotelData.results.push(highestEcstasyData);
+		providerData[highestEcstasyProvider]['index']++;
 	}
 
-	Promise.all(promises).then(mergeData);
+	return hotelData;
 }
